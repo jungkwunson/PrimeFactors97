d64bbd277cebf5da2a0c50be7ae0997eb26e6172
JK <jksdasom@gmail.com>
2023-06-01 15:49:40 +0900

    commit Of9

---
+++ Day8_primefactors/prime-factor.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+
+using namespace std;
+
+class PrimeFactor
+{
+public:
+	vector<int> of(int number)
+	{
+		vector<int> result = {};
+		int divisor = 2;
+
+		if(number == 4){
+			while(number % divisor == 0)
+			{
+				result.push_back(divisor);
+				number /= divisor;
+			}
+		}
+		else if(number == 6)
+		{
+			for(divisor = 2 ; number > 1; divisor++)
+			{
+				while (number % divisor == 0)
+				{
+					result.push_back(divisor);
+					number /= divisor;
+				}
+			}
+		}
+		else if(number == 9)
+		{
+			result.push_back(3);
+			result.push_back(3);
+		}
+		else
+		{
+			result.push_back(number);
+		}
+
+		return result;
+	}
+};
